fc808478da0ce723d4bfdc5b4ebfd189b8a43b3e
Leandr Lopez <denisfotoman@yandex.ru>
2022-12-10 21:33:41 +0300

    latest revision

--- Navigation/PostViewController.swift
+++ Navigation/PostViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 class PostViewController: UIViewController {
     
     var post: Post?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         title = "Posts"
         
         
@@ -25,15 +25,5 @@ class PostViewController: UIViewController {
     }
     
     @objc func press() { present(InfoViewController(), animated: true) }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
